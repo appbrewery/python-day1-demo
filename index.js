ebf8afeece61865d2f59207319dc90d2a9c9afa8
Angela Yu <8798027+angelabauer@users.noreply.github.com>
2024-03-05 14:38:32 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,88 @@
+//Current line
+var CurrentId = undefined;
+
+var inputValues = [];
+var bandName = "";
+const inputPrompts = ["What's your pet's name?"];
+let isFirstClick = true;
+
+//Click Run
+$(document).ready(function () {
+  $("#run-button").click(function () {
+    inputValues = [];
+    var bandName = "";
+    $("#Content").empty();
+    NewLine("Welcome to the Band Name Generator.", false);
+    NewLine("What's the name of the city you grew up in?", true);
+  });
+});
+
+//Enter button
+$(document).on("keydown", function (e) {
+  var x = event.which || event.keyCode;
+  if (x === 13 || x == 13) {
+    var consoleLine = $("#" + CurrentId + " input").val();
+
+    console.log(consoleLine);
+    bandName += consoleLine;
+    inputValues.push({ id: CurrentId, val: consoleLine });
+
+    console.log(inputValues);
+
+    if (inputValues.length > inputPrompts.length) {
+      console.log("called");
+      NewLine("Your band name could be " + bandName, false);
+
+      $(".console-carrot").remove();
+      return;
+    }
+
+    $(".console-carrot").remove();
+
+    NewLine(inputPrompts[inputValues.length - 1], true);
+    // setTimeout(NewLine, delay);
+  }
+});
+$(document).on("keydown", function (e) {
+  var x = event.which || event.keyCode;
+  var line = $("#" + CurrentId + " input");
+  var length = line.val().length;
+  if (x != 8) {
+    line.attr("size", 1 + length);
+  } else {
+    line.attr("size", length * 0.95);
+  }
+  if (length === 0) {
+    $("#" + CurrentId + " input").attr("size", "1");
+  }
+});
+$(document).on("click", function (e) {
+  $("#" + CurrentId + " input").focus();
+});
+
+//New line
+function NewLine(text, isPrompt) {
+  if (CurrentId !== undefined) {
+    $("#" + CurrentId + " input").prop("disabled", true);
+  }
+  CurrentId = "consoleInput-" + GenerateId();
+
+  if (isPrompt) {
+    //New line input
+    $("#Content").append("<div>" + text + "</div>");
+    $("#Content").append(
+      '<div id="' +
+        CurrentId +
+        '">' +
+        '<input autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false" type="text" class="terminal-input" /><div class="console-carrot"></div></div>'
+    );
+    $("#" + CurrentId + " input").focus();
+    $("#" + CurrentId + " input").attr("size", "1");
+  } else {
+    $("#Content").append('<div id="' + CurrentId + '">' + text + "</div>");
+  }
+}
+
+function GenerateId() {
+  return Math.random().toString(16).slice(2);
+}
